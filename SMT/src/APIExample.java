236c18d3d9860e22c5bd138b3391e16251a5b08b
dcok <dcok@localhost>
2011-06-01 20:47:13 -0400

    Added capability of writing and reading old Z3 constant bitvector syntax, which enabled some tests to work.

--- SMT/src/APIExample.java
+++ SMT/src/APIExample.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 import org.smtlib.*;
 import org.smtlib.command.C_declare_fun;
 import org.smtlib.impl.Script;
@@ -11,8 +14,10 @@ public class APIExample {
 		try {
 			// Parsing from a string
 			IExpr.IFactory efactory = smt.smtConfig.exprFactory;
-			ISource source = smt.smtConfig.smtFactory.createSource(new CharSequenceReader(new java.io.StringReader("(set-logic QF_UF)(declare-fun p () Bool)")),null);
+			ISource source = smt.smtConfig.smtFactory.createSource(new CharSequenceReader(new java.io.StringReader("(set-option :produce-models true)(set-logic QF_UF)(declare-fun p () Bool)")),null);
 			IParser parser = smt.smtConfig.smtFactory.createParser(smt.smtConfig,source);
+			ICommand command0 = parser.parseCommand();
+			System.out.println(smt.smtConfig.defaultPrinter.toString(command0));
 			ICommand command1 = parser.parseCommand();
 			ICommand command2 = parser.parseCommand();
 			if (!parser.isEOD()) {
@@ -63,7 +68,6 @@ public class APIExample {
 			response = script.execute(solver);
 			System.out.println(printer.toString(response));
 			
-			
 			// Execute commands directly
 			// THIS API WILL BE CHANGING
 			ISort.IFactory sortfactory = smt.smtConfig.sortFactory;
@@ -79,7 +83,36 @@ public class APIExample {
 			// comment on toString directly on ASTs?t
 			
 			// typechecking?
+
+			// Bit-vector and model example
+			List<IExpr.INumeral> nums = new LinkedList<IExpr.INumeral>();
+			nums.add(efactory.numeral(32)); // TODO - room for improvement in ease of use here...
+			ISort bv32 = sortfactory.createSortExpression(efactory.id(efactory.symbol("BitVec"),nums));
+			solver = new org.smtlib.solvers.Solver_z3(smt.smtConfig,"C:/cygwin/home/dcok/mybin/z3.exe");
+			solver.start();
+			solver.set_option(efactory.keyword(":produce-models"),efactory.symbol("true"));
+			r = solver.set_logic("QF_BV",null);
+			r = solver.declare_fun(new C_declare_fun(efactory.symbol("pb"),new java.util.LinkedList<ISort>(),bv32));
+			r = solver.declare_fun(new C_declare_fun(efactory.symbol("pc"),new java.util.LinkedList<ISort>(),bv32));
+
+			solver.push(1);
+			r = solver.assertExpr(
+					efactory.fcn(efactory.symbol("="),efactory.symbol("pc"),efactory.fcn(efactory.symbol("bvnot"),efactory.symbol("pb"))));
+			r = solver.check_sat();
+			System.out.println(printer.toString(r));
+			r = solver.get_value(efactory.symbol("pb"),efactory.symbol("pc"));
+			System.out.println(printer.toString(r));
 			
+			r = solver.assertExpr(
+					efactory.fcn(efactory.symbol("="),efactory.symbol("pb"),efactory.binary("01001010010100101001010010100111")));
+			r = solver.check_sat();
+			System.out.println(printer.toString(r));
+			r = solver.get_value(efactory.symbol("pb"),efactory.symbol("pc"));
+			System.out.println(printer.toString(r));
+			solver.pop(1);
+			solver.exit();
+			
+
 			
 		} catch (java.io.IOException e) {
 			// Can happen if the ISource is reading from a file
--- SMT/src/org/smtlib/Version.java
+++ SMT/src/org/smtlib/Version.java
@@ -1,5 +1,5 @@
 package org.smtlib;
 
 public interface Version {
-	String VERSION_ID = "jSMTLIB version 0.9.2";
+	String VERSION_ID = "jSMTLIB version 0.9.2a";
 }
