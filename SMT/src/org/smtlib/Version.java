b30dde4b3654c6d4ac50d7b19d705b9204304732
David Cok <davidcok@users.noreply.github.com>
2014-03-24 07:31:03 -0400

    Commits for version 0.9.10

--- SMT/src/org/smtlib/Version.java
+++ SMT/src/org/smtlib/Version.java
@@ -1,30 +1,18 @@
 package org.smtlib;
 
-import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 
 public class Version {
-	public static final String VERSION_ID = "jSMTLIB version " + version();
-	
     private static final String versionRBName = "org.smtlib.resources.version";
     private static ResourceBundle versionRB;
 
     public static String version() throws RuntimeException {
     	String key = "release";
-        if (versionRB == null) {
-            //try {
-                versionRB = ResourceBundle.getBundle(versionRBName);
-//            } catch (MissingResourceException e) {
-//                return Log.getLocalizedString("version.not.available");
-//            }
-        }
-//        try {
-            return versionRB.getString(key);
-//        }
-//        catch (MissingResourceException e) {
-//            return Log.getLocalizedString("version.not.available");
-//        }
+    	if (versionRB == null) {
+    		versionRB = ResourceBundle.getBundle(versionRBName);
+    	}
+    	return versionRB.getString(key);
     }
 
 }
