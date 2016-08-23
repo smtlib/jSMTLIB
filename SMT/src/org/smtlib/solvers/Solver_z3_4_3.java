832e1a516f503e78a3863c5c8104c4c6a38db90e
iconmaster5326 <crazybonesj@yahoo.com>
2016-08-23 09:45:32 -0400

    Fixed bug with Z3 4.3.2 returning multiple results for (push)

--- SMT/src/org/smtlib/solvers/Solver_z3_4_3.java
+++ SMT/src/org/smtlib/solvers/Solver_z3_4_3.java
@@ -355,10 +355,16 @@ public class Solver_z3_4_3 extends AbstractSolver implements ISolver {
 		if (number == 0) return smtConfig.responseFactory.success();
 		try {
 			pushesDepth += number;
-			IResponse r = parseResponse(solverProcess.sendAndListen("(push ",Integer.toString(number),")\n"));
-			// FIXME - actually only see this problem on Linux
-			if (r.isError() && !isWindows) return successOrEmpty(smtConfig);
-			return r;
+			// This odd invocation is to correct a bug in Z3 4.3.2, where (push) can print out more than one success message.
+			solverProcess.sendNoListen("(push ",Integer.toString(number),")\n");
+			solverProcess.sendNoListen("(echo \"<<DONE>>\")\n");
+			String s;
+			do {
+				s = solverProcess.listen();
+			} while (!s.contains("<<DONE>>"));
+			// FIXME: If an error occurs, this will loop forever.
+			// We can't use parseResponse to see if it an error, as the function does not expect Z3's buggy output.
+			return successOrEmpty(smtConfig);
 		} catch (Exception e) {
 			return smtConfig.responseFactory.error("Error writing to Z3 solver: " + e);
 		}
