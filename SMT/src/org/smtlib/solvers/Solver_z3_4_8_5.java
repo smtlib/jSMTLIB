36865624857278a2c751c3e71ece15dc66e11fb2
Malte Mues <mail.mues@gmail.com>
2020-12-10 16:20:31 +0100

    Enable solver loading from jar and use the shipped solver in APIExample

--- SMT/src/org/smtlib/solvers/Solver_z3_4_8_5.java
+++ SMT/src/org/smtlib/solvers/Solver_z3_4_8_5.java
@@ -1,27 +1,76 @@
 package org.smtlib.solvers;
 
-import org.smtlib.IParser;
-import org.smtlib.IResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.Set;
 import org.smtlib.SMT;
-import org.smtlib.impl.Pos;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Solver_z3_4_8_5 extends Solver_z3_4_5 {
-	protected String NAME_VALUE = "z3-4.8.5";
+  private static String resolvedExecutable = "";
+  protected String NAME_VALUE = "z3-4.8.5";
 
-	/**
-	 * Creates an instance of the Z3 solver
-	 */
-	public Solver_z3_4_8_5(final SMT.Configuration smtConfig, /*@NonNull*/ final String executable) {
-		super(smtConfig, executable);
-	}
+  /** Creates an instance of the Z3 solver */
+  public Solver_z3_4_8_5(final SMT.Configuration smtConfig, /*@NonNull*/ final String executable) {
+    super(smtConfig, executable);
+  }
 
-	/**
-	 * Creates an instance of the Z3 solver
-	 */
-	public Solver_z3_4_8_5(final SMT.Configuration smtConfig, /*@NonNull*/ final String[] command) {
-		super(smtConfig, command);
-	}
+  public Solver_z3_4_8_5(final SMT.Configuration smtConfig) {
+    this(smtConfig, resolveExecutable());
+  }
+
+  /** Creates an instance of the Z3 solver */
+  public Solver_z3_4_8_5(final SMT.Configuration smtConfig, /*@NonNull*/ final String[] command) {
+    super(smtConfig, command);
+  }
+
+  private static String resolveExecutable() {
+    final String os = System.getProperty("os.name").toLowerCase();
+    final InputStream executable;
+    final ClassLoader loader = Solver_z3_4_8_5.class.getClassLoader();
+    String executableName = "z3-4.8.5";
+    if (!resolvedExecutable.equals("")) {
+      return resolvedExecutable;
+    }
+    if (os.contains("win")) {
+      executable = loader.getResourceAsStream("windows/z3-4.8.5/z3-4.8.5.exe");
+      executableName += ".exe";
+    } else if (os.contains("mac")) {
+      executable = loader.getResourceAsStream("mac/z3-4.8.5/z3-4.8.5.macosx");
+      executableName += ".macosx";
+    } else {
+      executable = loader.getResourceAsStream("linux/z3-4.8.5/z3-4.8.5.linux");
+      executableName += ".linux";
+    }
+    try {
+      Path tmpSolverDir = Files.createTempDirectory("jSMTLIB-solver");
+      Runtime.getRuntime().addShutdownHook(deleteTempDirWithContent(tmpSolverDir));
+      Path newSolverPath = Paths.get(tmpSolverDir.toString(), executableName);
+      Files.copy(executable, newSolverPath);
+      executable.close();
+      Set<PosixFilePermission> perms = Files.getPosixFilePermissions(newSolverPath);
+      perms.add(PosixFilePermission.OWNER_EXECUTE);
+      Files.setPosixFilePermissions(newSolverPath, perms);
+      resolvedExecutable = newSolverPath.toString();
+      return resolvedExecutable;
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException("Failed to create Z3 4.8.5");
+    }
+  }
+
+  private static Thread deleteTempDirWithContent(Path tmpSolverDir) {
+    return new Thread(
+        () -> {
+          File tmpDir = tmpSolverDir.toFile();
+          for (File f : tmpDir.listFiles()) {
+            f.delete();
+          }
+          tmpDir.delete();
+        });
+  }
 }
